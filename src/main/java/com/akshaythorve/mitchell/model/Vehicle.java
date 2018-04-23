9975114b7b1a013016b431b13f81c67727c402cf
Akshay Thorve <thorveakshay@gmail.com>
2018-04-22 19:39:27 -0700

    adding @JsonProperty for Spring REST consuming JSON uppercase letters

--- src/main/java/com/akshaythorve/mitchell/model/Vehicle.java
+++ src/main/java/com/akshaythorve/mitchell/model/Vehicle.java
@@ -4,14 +4,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Entity
 public class Vehicle {
 	
+	
+	/* adding @JsonProperty for Spring REST consuming JSON uppercase letters
+*/
+	
 	@Id
 	@GeneratedValue
+	@JsonProperty("Id")
 	private int Id;
+	
+	@JsonProperty("Year")
 	private int Year;
+	@JsonProperty("Make")
 	private String Make;
+	
+	@JsonProperty("Model")
 	private String Model;
 
     public Vehicle() {
@@ -33,34 +45,42 @@ public class Vehicle {
 		Model = model;
 	}
 	
+	@JsonProperty("Id")
 	public int getId() {
         return Id;
     }
 
+	@JsonProperty("Id")
     public void setId(int id) {
         Id = id;
     }
 
+	@JsonProperty("Year")
     public int getYear() {
         return Year;
     }
-
+    
+    @JsonProperty("Year")
     public void setYear(int year) {
         Year = year;
     }
 
+    @JsonProperty("Make")
     public String getMake() {
         return Make;
     }
 
+    @JsonProperty("Make")
     public void setMake(String make) {
         Make = make;
     }
 
+    @JsonProperty("Model")
     public String getModel() {
         return Model;
     }
 
+    @JsonProperty("Model")
     public void setModel(String model) {
         Model = model;
     }
