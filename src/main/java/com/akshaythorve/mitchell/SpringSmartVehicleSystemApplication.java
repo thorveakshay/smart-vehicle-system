6e4c2f61e2c105b3aca9e5e617a4297c241b3cbb
Akshay Thorve <thorveakshay@gmail.com>
2018-04-22 19:17:40 -0700

    adding CORS support

--- src/main/java/com/akshaythorve/mitchell/SpringSmartVehicleSystemApplication.java
+++ src/main/java/com/akshaythorve/mitchell/SpringSmartVehicleSystemApplication.java
@@ -1,19 +1,33 @@
 package com.akshaythorve.mitchell;
 
-import com.akshaythorve.mitchell.model.Vehicle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.akshaythorve.mitchell.model.Vehicle;
 import com.akshaythorve.mitchell.repository.VehicleRepository;
 
 @SpringBootApplication
-public class SpringSmartVehicleSystemApplication {
+@EnableWebMvc
+@Configuration
+public class SpringSmartVehicleSystemApplication extends WebMvcConfigurerAdapter {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SpringSmartVehicleSystemApplication.class);
+	
+	
+	// added to support for CORS i.e multi-domain access
+		@Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**");
+	    }
+		
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSmartVehicleSystemApplication.class, args);
